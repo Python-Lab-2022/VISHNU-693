3c72ca2e868150d6a126a2fc50a7f98448ec589c
VISHNU-693 <115703023+VISHNU-693@users.noreply.github.com>
2022-12-22 13:01:05 +0530

    Add files via upload

---
+++ prgrm1.py
@@ -0,0 +1,11 @@
+print ("Current Year is")
+import datetime
+today = datetime.date.today()
+year = today.year
+print(year)
+print ("Enter Last Year")
+endYear = int(input())
+print ("List of Leap Years")
+for year in range(year, endYear):
+  if (0 == year % 4) and (0 != year % 100) or (0 == year % 400):
+    print (year)
